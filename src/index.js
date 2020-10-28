f29538eb47c773930024a052bef46ad81b5753f3
lagerros <jacobklagerros@gmail.com>
2020-10-27 22:35:19 -0700

    test

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ const getPageData = pageName => {
 	return window.roamAlphaAPI.q(queryString, pageName)
 }
 
-isStubgPage(zoo)
+isStubPage(zoo)
 
 const objToString = obj => {
     /** Turns a given roam object into a single string representation **/
